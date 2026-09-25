72ed285a9e6522d8f544412775012e654f6d365e
darknesslord19 <admin.lord19@gmail.com>
2026-09-26 01:55:36 +0300

    Refactor header initialization and clean up code

--- CanliTV/src/main/kotlin/com/keyiflerolsun/CanliTV.kt
+++ CanliTV/src/main/kotlin/com/keyiflerolsun/CanliTV.kt
@@ -38,7 +38,6 @@ class CanliTV : MainAPI() {
 
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
         val headers = mapOf(
-                val headers = mapOf(
             "User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
             "Referer" to "https://tvheryerde.com",
             "Origin" to "https://tvheryerde.com",
@@ -47,20 +46,19 @@ class CanliTV : MainAPI() {
             "Authorization" to "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbnYiOiJMSVZFIiwiaXBiIjoiMCIsImNnZCI6IjA5M2Q3MjBhLTUwMmMtNDFlZC1hODBmLTJiODE2OTg0ZmI5NSIsImNzaCI6IlRSS1NUIiwiZGN0IjoiM0VGNzUiLCJkaSI6IjNkY2I2NmJiLTZhNjctNDIwYi1iN2MyLTg3ZGQ2MGFjNDNjZCIsInNnZCI6Ijk1N2U3NjliLWJiYjgtNGFiMC05NzYwLTgyM2UyMGE1OWFlMyIsInNwZ2QiOiIxNTY0ODUxZC1hY2ViLTQyZWUtYjkwZi04MGFlNTczOGEyM2EiLCJpY2giOiIwIiwiaWRtIjoiMCIsImlhIjoiOjpmZmZmOjEwLjAuMC42IiwiYXB2IjoiMS4wLjAiLCJhYm4iOiIxMDAwIiwibmJmIjoxNzQwOTY1ODI4LCJleHAiOjE3NDA5NjU4ODgsImlhdCI6MTc0MDk2NTgyOH0.8SgjsXtcwvmCYpV0W2T-rwwUiiFKpluz8crfpRhDv9A"
         )
 
-
         // app.get kullanarak gelen veriyi doğrudan metin olarak alıyoruz (OkHttp gzip'i otomatik çözer)
         val responseText = app.get(mainUrl, headers = headers).text
-        
+
         val objectMapper = ObjectMapper().registerModule(KotlinModule.Builder().build())
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
         val result: CanliTvResult = objectMapper.readValue(responseText)
-        
+
         val liste = mutableListOf<HomePageList>()
         kanallar.clear() // Tekrar tekrar ana sayfaya girildiğinde listelerin üst üste binmemesi için temizliyoruz
         kanallar.addAll(result.dataResult.allChannels!!)
-        
+
         val newHomePageResponse = newHomePageResponse(
-            kanallar.groupBy{ it.categories!![0].name }.filter{ it.key != "Bilgilendirme" }.map { group ->
+            kanallar.groupBy { it.categories!![0].name }.filter { it.key != "Bilgilendirme" }.map { group ->
                 val title = group.key ?: ""
                 val show = group.value.map { kanal ->
                     val streamurl = kanal.streamData!!.hlsStreamUrl.toString()
